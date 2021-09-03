779b00f19daae08d4d6e0f0a914446e35fdc2ab6
Krzysztof Skubała <Krzysztof.Skubala@cshark.com>
2021-09-03 12:22:33 +0200

    Clickable profile button menu

---
+++ Peernet.Browser.Application/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MvvmCross.ViewModels;
+
+namespace Peernet.Browser.Application.ViewModels
+{
+    public class MenuItemViewModel : MvxViewModel
+    {
+        public MenuItemViewModel(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; }
+    }
+}
--- Peernet.Browser.Application/ViewModels/NavigationBarViewModel.cs
+++ Peernet.Browser.Application/ViewModels/NavigationBarViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MvvmCross.Commands;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Peernet.Browser.Application.ViewModels
@@ -8,10 +9,30 @@ namespace Peernet.Browser.Application.ViewModels
     public class NavigationBarViewModel : MvxViewModel
     {
         private readonly IMvxNavigationService navigationService;
+        private bool isProfileMenuVisible;
+
+        public bool IsProfileMenuVisible
+        {
+            get => isProfileMenuVisible;
+            set
+            {
+                isProfileMenuVisible = value;
+                RaisePropertyChanged(nameof(IsProfileMenuVisible));
+            }
+        }
+
+        public List<MenuItemViewModel> Items { get; set; }
+
 
         public NavigationBarViewModel(IMvxNavigationService navigationService)
         {
             this.navigationService = navigationService;
+            Items = new List<MenuItemViewModel>
+            {
+                new MenuItemViewModel("About"),
+                new MenuItemViewModel("FAQ (Help)"),
+                new MenuItemViewModel("Backup to a file")
+            };
         }
 
         public IMvxAsyncCommand NavigateHomeCommand
@@ -47,12 +68,14 @@ namespace Peernet.Browser.Application.ViewModels
             }
         }
 
-        public IMvxAsyncCommand NavigateSettingsCommand
+        public IMvxAsyncCommand OpenProfileMenuCommand
         {
             get
             {
                 return new MvxAsyncCommand(() =>
                 {
+                    IsProfileMenuVisible ^= true;
+
                     return Task.CompletedTask;
                 });
             }
