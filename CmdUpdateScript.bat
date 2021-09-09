67b66a2cd1475b9fec55a463f57c00fa02e0f08f
MIchał Jędruch <michal.jedruch@cshark.com>
2021-09-09 12:14:34 +0200

    try to autos CMD update process to Tools

---
+++ CmdUpdateScript.bat
@@ -0,0 +1,5 @@
+cd../../Cmd
+git pull
+go build
+rmdir ..\Browser\Peernet.Browser.Infrastructure\Tools\CmdSource /s /q
+xcopy /s ..\Cmd\ ..\Browser\Peernet.Browser.Infrastructure\Tools\CmdSource\ /e
