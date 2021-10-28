c638af51bb4d4134c737a33755a7be2c14c915c1
MIchał Jędruch <michal.jedruch@cshark.com>
2021-10-28 09:55:49 +0200

    fix about back button

--- Peernet.Browser.Application/ViewModels/AboutViewModel.cs
+++ Peernet.Browser.Application/ViewModels/AboutViewModel.cs
@@ -1,8 +1,16 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
 
 namespace Peernet.Browser.Application.ViewModels
 {
     public class AboutViewModel : MvxViewModel
     {
+        public AboutViewModel(IMvxNavigationService navigationService)
+        {
+            BackCommand = new MvxCommand(() => navigationService.Close(this));
+        }
+
+        public IMvxCommand BackCommand { get; }
     }
 }
