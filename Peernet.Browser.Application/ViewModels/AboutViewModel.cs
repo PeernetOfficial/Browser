ddcce9e428503ee281b26eb92bb3eedf6cebf1aa
MIchał Jędruch <michal.jedruch@cshark.com>
2021-11-02 09:39:42 +0100

    remove about

--- Peernet.Browser.Application/ViewModels/AboutViewModel.cs
+++ Peernet.Browser.Application/ViewModels/AboutViewModel.cs
@@ -1,16 +1,8 @@
-﻿using MvvmCross.Commands;
-using MvvmCross.Navigation;
-using MvvmCross.ViewModels;
+﻿using MvvmCross.ViewModels;
 
 namespace Peernet.Browser.Application.ViewModels
 {
     public class AboutViewModel : MvxViewModel
     {
-        public AboutViewModel(IMvxNavigationService navigationService)
-        {
-            BackCommand = new MvxCommand(() => navigationService.Close(this));
-        }
-
-        public IMvxCommand BackCommand { get; }
     }
 }
