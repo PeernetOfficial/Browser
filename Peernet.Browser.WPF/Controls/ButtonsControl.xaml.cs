e07e66bd37a2bfc37bc7fa73c213f4fbe396f1b3
Krzysztof Skubała <Krzysztof.Skubala@cshark.com>
2022-03-15 17:38:01 +0100

    Fix windows bar event capturing so it properly uses WindowChrome

--- Peernet.Browser.WPF/Controls/ButtonsControl.xaml.cs
+++ Peernet.Browser.WPF/Controls/ButtonsControl.xaml.cs
@@ -8,17 +8,5 @@ namespace Peernet.Browser.WPF.Controls
     public partial class ButtonsControl : UserControl
     {
         public ButtonsControl() => InitializeComponent();
-
-        private void UserControl_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if (System.Windows.Application.Current.MainWindow.WindowState != System.Windows.WindowState.Maximized)
-            {
-                System.Windows.Application.Current.MainWindow.WindowState = System.Windows.WindowState.Maximized;
-            }
-            else
-            {
-                System.Windows.Application.Current.MainWindow.WindowState = System.Windows.WindowState.Normal;
-            }
-        }
     }
 }
